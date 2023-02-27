b610af304776365c72f482a22637ee96b1cc4a3c
Saulo Alves Magalhães <saulo.magalhaes@outlook.com.br>
2023-02-27 11:53:32 -0300

    feat: adiciona o automapper a application

---
+++ src/Backend/MinhasReceitas.Application/Servicos/Automapper/AutomapperConfiguracao.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using MinhasReceitas.Communication.Requisicoes;
+using MinhasReceitas.Domain.Entidades;
+
+namespace MinhasReceitas.Application.Servicos.Automapper;
+
+public class AutomapperConfiguracao : Profile
+{
+    public AutomapperConfiguracao()
+    {
+        CreateMap<RequisicaoRegistrarUsuarioJson, Usuario>()
+            .ForMember(destino => destino.Senha, config => config.Ignore()); 
+    }
+}
